49602a5f06f0f8de7cf8f8c6c4f3ab80eed180d3
Danny Milosavljevic <danny_milo@yahoo.com>
2005-05-03 22:37:44 +0000

    AboutDialog fixes
    (Old svn revision: 13912)

--- gui/aboutdialog.c
+++ gui/aboutdialog.c
@@ -27,14 +27,204 @@ PyTypeObject PyXfceAboutDialog_Type;
 /* ----------- XfceAboutDialog ----------- */
 
 static int
-pygobject_no_constructor(PyObject *self, PyObject *args, PyObject *kwargs)
+_wrap_xfce_about_dialog_new_normal(PyGObject *self, PyObject *args, PyObject *kwargs)
 {
-    gchar buf[512];
+    GType obj_type = pyg_type_from_object((PyObject *) self);
+    static char* kwlist[] = { NULL };
 
-    g_snprintf(buf, sizeof(buf), "%s is an abstract widget", self->ob_type->tp_name);
-    PyErr_SetString(PyExc_NotImplementedError, buf);
-    return -1;
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, ":aboutdialog.AboutDialog.__init__", kwlist))
+        return -1;
+
+    self->obj = g_object_newv(obj_type, 0, NULL);
+    if (!self->obj) {
+        PyErr_SetString(PyExc_RuntimeError, "could not create %(typename)s object");
+        return -1;
+    }
+
+    pygobject_register_wrapper((PyObject *)self);
+    return 0;
 }
+
+
+static PyObject *
+_wrap_xfce_about_dialog_set_program(PyGObject *self, PyObject *args, PyObject *kwargs)
+{
+    static char *kwlist[] = { "value", NULL };
+    char *value;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s:XfceAboutDialog.set_program", kwlist, &value))
+        return NULL;
+    xfce_about_dialog_set_program(XFCE_ABOUT_DIALOG(self->obj), value);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *
+_wrap_xfce_about_dialog_set_version(PyGObject *self, PyObject *args, PyObject *kwargs)
+{
+    static char *kwlist[] = { "value", NULL };
+    char *value;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s:XfceAboutDialog.set_version", kwlist, &value))
+        return NULL;
+    xfce_about_dialog_set_version(XFCE_ABOUT_DIALOG(self->obj), value);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *
+_wrap_xfce_about_dialog_set_description(PyGObject *self, PyObject *args, PyObject *kwargs)
+{
+    static char *kwlist[] = { "value", NULL };
+    char *value;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s:XfceAboutDialog.set_description", kwlist, &value))
+        return NULL;
+    xfce_about_dialog_set_description(XFCE_ABOUT_DIALOG(self->obj), value);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *
+_wrap_xfce_about_dialog_set_copyright(PyGObject *self, PyObject *args, PyObject *kwargs)
+{
+    static char *kwlist[] = { "value", NULL };
+    char *value;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s:XfceAboutDialog.set_copyright", kwlist, &value))
+        return NULL;
+    xfce_about_dialog_set_copyright(XFCE_ABOUT_DIALOG(self->obj), value);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *
+_wrap_xfce_about_dialog_set_license(PyGObject *self, PyObject *args, PyObject *kwargs)
+{
+    static char *kwlist[] = { "value", NULL };
+    char *value;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s:XfceAboutDialog.set_license", kwlist, &value))
+        return NULL;
+    xfce_about_dialog_set_license(XFCE_ABOUT_DIALOG(self->obj), value);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *
+_wrap_xfce_about_dialog_set_homepage(PyGObject *self, PyObject *args, PyObject *kwargs)
+{
+    static char *kwlist[] = { "value", NULL };
+    char *value;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "s:XfceAboutDialog.set_homepage", kwlist, &value))
+        return NULL;
+    xfce_about_dialog_set_homepage(XFCE_ABOUT_DIALOG(self->obj), value);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *
+_wrap_xfce_about_dialog_add_credit(PyGObject *self, PyObject *args, PyObject *kwargs)
+{
+    static char *kwlist[] = { "name", "mail", "task", NULL };
+    char *name, *mail, *task;
+
+    if (!PyArg_ParseTupleAndKeywords(args, kwargs, "sss:XfceAboutDialog.add_credit", kwlist, &name, &mail, &task))
+        return NULL;
+    xfce_about_dialog_add_credit(XFCE_ABOUT_DIALOG(self->obj), name, mail, task);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *
+_wrap_xfce_about_dialog_get_program(PyGObject *self)
+{
+    const gchar *ret;
+
+    ret = xfce_about_dialog_get_program(XFCE_ABOUT_DIALOG(self->obj));
+    if (ret)
+        return PyString_FromString(ret);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *
+_wrap_xfce_about_dialog_get_version(PyGObject *self)
+{
+    const gchar *ret;
+
+    ret = xfce_about_dialog_get_version(XFCE_ABOUT_DIALOG(self->obj));
+    if (ret)
+        return PyString_FromString(ret);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *
+_wrap_xfce_about_dialog_get_description(PyGObject *self)
+{
+    const gchar *ret;
+
+    ret = xfce_about_dialog_get_description(XFCE_ABOUT_DIALOG(self->obj));
+    if (ret)
+        return PyString_FromString(ret);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *
+_wrap_xfce_about_dialog_get_copyright(PyGObject *self)
+{
+    const gchar *ret;
+
+    ret = xfce_about_dialog_get_copyright(XFCE_ABOUT_DIALOG(self->obj));
+    if (ret)
+        return PyString_FromString(ret);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *
+_wrap_xfce_about_dialog_get_license(PyGObject *self)
+{
+    const gchar *ret;
+
+    ret = xfce_about_dialog_get_license(XFCE_ABOUT_DIALOG(self->obj));
+    if (ret)
+        return PyString_FromString(ret);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyObject *
+_wrap_xfce_about_dialog_get_homepage(PyGObject *self)
+{
+    const gchar *ret;
+
+    ret = xfce_about_dialog_get_homepage(XFCE_ABOUT_DIALOG(self->obj));
+    if (ret)
+        return PyString_FromString(ret);
+    Py_INCREF(Py_None);
+    return Py_None;
+}
+
+static PyMethodDef _PyXfceAboutDialog_methods[] = {
+    { "set_program", (PyCFunction)_wrap_xfce_about_dialog_set_program, METH_VARARGS|METH_KEYWORDS },
+    { "set_version", (PyCFunction)_wrap_xfce_about_dialog_set_version, METH_VARARGS|METH_KEYWORDS },
+    { "set_description", (PyCFunction)_wrap_xfce_about_dialog_set_description, METH_VARARGS|METH_KEYWORDS },
+    { "set_copyright", (PyCFunction)_wrap_xfce_about_dialog_set_copyright, METH_VARARGS|METH_KEYWORDS },
+    { "set_license", (PyCFunction)_wrap_xfce_about_dialog_set_license, METH_VARARGS|METH_KEYWORDS },
+    { "set_homepage", (PyCFunction)_wrap_xfce_about_dialog_set_homepage, METH_VARARGS|METH_KEYWORDS },
+    { "add_credit", (PyCFunction)_wrap_xfce_about_dialog_add_credit, METH_VARARGS|METH_KEYWORDS },
+    { "get_program", (PyCFunction)_wrap_xfce_about_dialog_get_program, METH_NOARGS },
+    { "get_version", (PyCFunction)_wrap_xfce_about_dialog_get_version, METH_NOARGS },
+    { "get_description", (PyCFunction)_wrap_xfce_about_dialog_get_description, METH_NOARGS },
+    { "get_copyright", (PyCFunction)_wrap_xfce_about_dialog_get_copyright, METH_NOARGS },
+    { "get_license", (PyCFunction)_wrap_xfce_about_dialog_get_license, METH_NOARGS },
+    { "get_homepage", (PyCFunction)_wrap_xfce_about_dialog_get_homepage, METH_NOARGS },
+    { NULL, NULL, 0 }
+};
 
 PyTypeObject PyXfceAboutDialog_Type = {
     PyObject_HEAD_INIT(NULL)
@@ -66,7 +256,7 @@ PyTypeObject PyXfceAboutDialog_Type = {
     offsetof(PyGObject, weakreflist),             /* tp_weaklistoffset */
     (getiterfunc)0,		/* tp_iter */
     (iternextfunc)0,	/* tp_iternext */
-    NULL,			/* tp_methods */
+    _PyXfceAboutDialog_methods,			/* tp_methods */
     0,					/* tp_members */
     0,		       	/* tp_getset */
     NULL,				/* tp_base */
@@ -74,7 +264,7 @@ PyTypeObject PyXfceAboutDialog_Type = {
     (descrgetfunc)0,	/* tp_descr_get */
     (descrsetfunc)0,	/* tp_descr_set */
     offsetof(PyGObject, inst_dict),                 /* tp_dictoffset */
-    (initproc)pygobject_no_constructor,		/* tp_init */
+    (initproc)_wrap_xfce_about_dialog_new_normal,		/* tp_init */
     (allocfunc)0,           /* tp_alloc */
     (newfunc)0,               /* tp_new */
     (freefunc)0,             /* tp_free */
@@ -125,6 +315,6 @@ pyaboutdialog_register_classes(PyObject *d)
     }
 
 
-#line 129 "aboutdialog.c"
+#line 319 "aboutdialog.c"
     pygobject_register_class(d, "XfceAboutDialog", XFCE_TYPE_ABOUT_DIALOG, &PyXfceAboutDialog_Type, Py_BuildValue("(O)", &PyGtkDialog_Type));
 }
