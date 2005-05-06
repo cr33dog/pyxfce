d7fe32bb954befc89d3dd0096cfc469ccace7380
Danny Milosavljevic <danny_milo@yahoo.com>
2005-05-06 07:46:02 +0000

    use a rodent icon name for now
    (Old svn revision: 14128)

--- gui/tests/testicontheme.py
+++ gui/tests/testicontheme.py
@@ -7,9 +7,9 @@ import gtk
 import xfce4
 
 widget = xfce4.gui.IconTheme(gtk.gdk.screen_get_default())
-ic = widget.load("folder", 24)
+ic = widget.load("xfce-mail", 24)
 print ic
-icname = widget.lookup("folder", 24)
+icname = widget.lookup("xfce-mail", 24)
 print icname
 
 image = gtk.Image()
