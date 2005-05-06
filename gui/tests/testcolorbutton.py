730f8259a7fbf03f185085199ce2efa00e1cc891
Danny Milosavljevic <danny_milo@yahoo.com>
2005-05-06 06:48:16 +0000

    update tests
    (Old svn revision: 14100)

---
+++ gui/tests/testcolorbutton.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# doesnt work
+import pygtk
+pygtk.require("2.0")
+import gtk
+import xfce4
+
+widget = xfce4.gui.Colorbutton()
+widget.show()
+
+w = gtk.Window()
+w.connect("destroy", lambda x: gtk.main_quit())
+
+w.set_border_width(7)
+w.add(widget)
+
+w.show()
+
+gtk.main()
