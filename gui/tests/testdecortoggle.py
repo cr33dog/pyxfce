af8d9e93c46c5ee168f3e35637c3e685035a4a2f
Danny Milosavljevic <danny_milo@yahoo.com>
2005-05-06 06:49:50 +0000

    add test
    (Old svn revision: 14101)

---
+++ gui/tests/testdecortoggle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# what is that for
+
+import pygtk
+pygtk.require("2.0")
+import gtk
+import xfce4
+
+widget = xfce4.gui.Decortoggle("right")  # "close"
+widget.show()
+
+w = gtk.Window()
+w.connect("destroy", lambda x: gtk.main_quit())
+
+w.set_border_width(7)
+w.add(widget)
+
+w.show()
+
+gtk.main()
