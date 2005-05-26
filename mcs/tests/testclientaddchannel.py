9ef3ffebc3090e2976a0a713a51504e6782b7696
Danny Milosavljevic <danny_milo@yahoo.com>
2005-05-26 22:17:10 +0000

    add tests for mcs
    (Old svn revision: 14557)

---
+++ mcs/tests/testclientaddchannel.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import xfce4
+mcsC = xfce4.mcs.Client()
+channel = mcsC.add_channel("X")
+
---
+++ mcs/tests/testclientshowdialog.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+import xfce4
+mcsC = xfce4.mcs.Client()
+
+mcsC.show_dialog("sound")
+
