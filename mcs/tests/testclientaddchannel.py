82622dcbacd819700162fbc228bf81bd8b1bfeff
Danny Milosavljevic <danny_milo@yahoo.com>
2005-05-27 16:06:02 +0000

    update mcs test a bit, adding callback
    (Old svn revision: 14568)

--- mcs/tests/testclientaddchannel.py
+++ mcs/tests/testclientaddchannel.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
+import pygtk
+pygtk.require("2.0")
+import gtk
+
+def setting_changed_cb(a,b,c):
+	print "X"
+
 import xfce4
 mcsC = xfce4.mcs.Client()
 channel = mcsC.add_channel("X")
+channel.connect("setting-changed", setting_changed_cb)
 
+gtk.main()
