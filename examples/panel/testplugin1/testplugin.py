9e3a341955700511e58fe55375b888b03faff87a
Danny Milosavljevic <danny_milo@yahoo.com>
2005-09-09 19:59:10 +0000

    add testplugin :)
    (Old svn revision: 17517)

---
+++ examples/panel/testplugin1/testplugin.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+
+import pygtk
+pygtk.require("2.0")
+import gtk
+
+import xfce4.panel
+
+class MyPlugin(xfce4.panel.ExternalPanelPlugin):
+	def __init__(self):
+		xfce4.panel.ExternalPanelPlugin.__init__(self)
+		
+		label = gtk.Label("test")
+		label.show()
+		
+		self.add(label)
+	
+
+# argc, argv, &info
+plugin = MyPlugin()
+if plugin == None:
+	# TODO exception
+	pass
+
+# plugin.run()
+
+plugin.connect("destroy", lambda x: gtk.main_quit())
+
+plugin.show()
+
+gtk.main()
+sys.exit(0)
