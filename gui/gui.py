10450974c99f5d5110b8fe4bf82cb96b4bb0f5be
Danny Milosavljevic <danny_milo@yahoo.com>
2006-01-06 23:25:36 +0000

    fix in_terminal
    (Old svn revision: 19326)

--- gui/gui.py
+++ gui/gui.py
@@ -8,7 +8,7 @@ import xfce4
 import gtk
 import os
 
-def spawn_xyz(spawner, in_terminal = False, use_startup_notification = True, screen = None, environment = None):
+def spawn_xyz(spawner, use_startup_notification = True, screen = None, environment = None):
   if screen == None:
     screen = gtk.gdk.screen_get_default()
     
@@ -39,8 +39,11 @@ def spawn_command_line_cb(command_line, new_environment):
   return ret
   
 def spawn_command_line(command_line, in_terminal = False, use_startup_notification = True, screen = None, environment = None):
-  return spawn_xyz(lambda x: spawn_command_line_cb(command_line + " &", x), in_terminal, use_startup_notification, screen, environment)
+  if in_terminal == True:
+    command_line = "xfterm4 -e " + command_line
+    
+  return spawn_xyz(lambda x: spawn_command_line_cb(command_line + " &", x), use_startup_notification, screen, environment)
 
-def spawn_args(args, in_terminal = False, use_startup_notification = True, screen = None, environment = None):
-  return spawn_xyz(lambda x: os.spawnle(os.P_NOWAIT, args[0], args, x), in_terminal, use_startup_notification, screen, environment)
+def spawn_args(args, use_startup_notification = True, screen = None, environment = None):
+  return spawn_xyz(lambda x: os.spawnle(os.P_NOWAIT, args[0], args, x), use_startup_notification, screen, environment)
 
